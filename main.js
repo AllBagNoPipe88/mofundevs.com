cb50059bd444178d70c646d06fe5d80bd985a33c
AllBagNoPipe88 <gaiagirl88@gmail.com>
2025-08-15 00:19:14 -0400

    Final draft before deploy

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 document.getElementById('menu-toggle').addEventListener('click', function () {
     document.getElementById('nav-links').classList.toggle('show');
 });
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextE1: '.swiper-button-next',
+        prevE1: '.swiper-button-prev'
+    },
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    speed: 2000
+
+});
